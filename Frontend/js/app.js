482c2786c253fd5c40239a5c910eaba81741f660
rodrigo.carneiro <rodrigo.carneiro@benner.com.br>
2024-10-17 14:49:35 -0300

    .NET Docker

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -73,6 +73,134 @@ function loadPoolInfo() {
     });
 }
 
+function loadSwapForm() {
+    $("#mainContainer").load("./swap.html", function () {
+        $("#alertClose").on("click", function (e) {
+            e.preventDefault();
+            $("#errorBox").hide();
+        });
+
+        $('input.numericInput').inputNumberFormat({
+            'decimal': 5,
+            'decimalAuto': 5,
+            'separator': '.',
+            'separatorAuthorized': ['.'], //['.', ','],
+            allowNegative: false
+          });
+          $('input.numericInput').on("input", function (e) {
+            let amount = parseFloat($("#origAmount").val());
+            let btcP = parseFloat($("#BtcProportion").val());
+            let stxP = parseFloat($("#StxProportion").val());
+            if ($("#title").data("coin") == "bitcoin") {
+                let price = btcP * amount;
+                $("#destAmount").val(price.toFixed(5));
+            }
+            else {
+                let price = stxP * amount;
+                $("#destAmount").val(price.toFixed(5));
+            }
+          });
+        $("#changeBtn").on("click", function (e) {
+            e.preventDefault();
+    
+            let amount = $("#origAmount").val();
+            $("#origAmount").val($("#destAmount").val());
+            $("#destAmount").val(amount);
+    
+            if ($("#title").data("coin") == "bitcoin") {
+                $("#origSelected").val("stacks");
+                $("#destSelected").val("bitcoin");
+                $("#title").text("STX To BTC");
+                $("p.description").text($("p.description").data("stacks"));
+    
+                $("#origPrice").text($("p.description").data("stxPrice"));
+                $("#destPrice").text($("p.description").data("btcPrice"));
+    
+                $("#title").data("coin", "stacks");
+            }
+            else {
+                $("#origSelected").val("bitcoin");
+                $("#destSelected").val("stacks");
+                $("#title").text("BTC To STX");
+                $("p.description").text($("p.description").data("bitcoin"));
+    
+                $("#origPrice").text($("p.description").data("btcPrice"));
+                $("#destPrice").text($("p.description").data("stxPrice"));
+    
+                $("#title").data("coin", "bitcoin");
+            }
+        });
+
+        $("#origSelected").on("change", function (e) {
+            $("#title").data("coin", ($(this).val() == "bitcoin") ? "stacks" : "bitcoin");
+            $("#changeBtn").trigger("click");
+        });
+
+        $("#destSelected").on("change", function (e) {
+            $("#title").data("coin", ($(this).val() == "bitcoin") ? "bitcoin" : "stacks");
+            $("#changeBtn").trigger("click");
+        });
+
+        $("#confirmBtn").on("click", function (e) {
+            e.preventDefault();
+    
+            let amountOrig = parseFloat($("#origAmount").val()) * 100000000;
+    
+            try {
+                window.LeatherProvider.request("sendTransfer", {
+                  recipients: [
+                    {
+                      address: $("#PoolBtcAddress").val(),
+                      amount: amountOrig,
+                    }
+                  ],
+                  network: "testnet",
+                }).then(function (response) {
+    
+                    alert(response.result.txid);
+                    console.log("Transaction ID:", response.result.txid);
+    
+                    let vOrigAmout = parseFloat($("#origAmount").val()) * 100000000;
+                    let vDestAmout = parseFloat($("#destAmount").val()) * 100000000;
+    
+                    let txParam = {
+                        btcToStx: true,
+                        btcAddress: $("#BtcAddress").val(),
+                        stxAddress: $("#StxAddress").val(),
+                        btcTxid: response.result.txid,
+                        stxTxid: null,
+                        btcAmount: vOrigAmout,
+                        stxAmount: vDestAmout
+                    };
+                    $.ajax({
+                        url: API_URL + "/api/Transaction/createTx",
+                        type: "PUT",
+                        data: JSON.stringify(txParam),
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function(response) {
+                            alert(JSON.stringify(response));
+                            console.log(response);
+                        },
+                        error: function(xhr, status, error) {
+                            doError(xhr.responseText);
+                            $("#openModalBtn").prop("disabled", true);
+                        }
+                    });
+                    //$("#btcAddress").val()
+                });
+              
+                //console.log("Response:", response);
+                //console.log("Transaction ID:", response.result.txid);
+              } catch (error) {
+                doError("Request error: " + error.error.message + "(" + error.error.code + ")");
+              }
+        });
+
+        loadPoolInfo();
+    });
+}
+
 (function(){
 
     /*
@@ -93,64 +221,6 @@ function loadPoolInfo() {
     }
     */
 
-    $("#alertClose").on("click", function (e) {
-        e.preventDefault();
-
-        $("#errorBox").hide();
-    });
-
-    $("#openModalBtn").prop("disabled", false);
-
-    $('input.numericInput').inputNumberFormat({
-        'decimal': 5,
-        'decimalAuto': 5,
-        'separator': '.',
-        'separatorAuthorized': ['.'], //['.', ','],
-        allowNegative: false
-      });
-      $('input.numericInput').on("input", function (e) {
-        let amount = parseFloat($("#origAmount").val());
-        let btcP = parseFloat($("#BtcProportion").val());
-        let stxP = parseFloat($("#StxProportion").val());
-        if ($("#title").data("coin") == "bitcoin") {
-            let price = btcP * amount;
-            $("#destAmount").val(price.toFixed(5));
-        }
-        else {
-            let price = stxP * amount;
-            $("#destAmount").val(price.toFixed(5));
-        }
-      });
-    $("#changeBtn").on("click", function (e) {
-        e.preventDefault();
-
-        let amount = $("#origAmount").val();
-        $("#origAmount").val($("#destAmount").val());
-        $("#destAmount").val(amount);
-
-        if ($("#title").data("coin") == "bitcoin") {
-            $("#origSelected").val("stacks");
-            $("#destSelected").val("bitcoin");
-            $("#title").text("STX To BTC");
-            $("p.description").text($("p.description").data("stacks"));
-
-            $("#origPrice").text($("p.description").data("stxPrice"));
-            $("#destPrice").text($("p.description").data("btcPrice"));
-
-            $("#title").data("coin", "stacks");
-        }
-        else {
-            $("#origSelected").val("bitcoin");
-            $("#destSelected").val("stacks");
-            $("#title").text("BTC To STX");
-            $("p.description").text($("p.description").data("bitcoin"));
-
-            $("#origPrice").text($("p.description").data("btcPrice"));
-            $("#destPrice").text($("p.description").data("stxPrice"));
-
-            $("#title").data("coin", "bitcoin");
-        }
-    });
     $("#connectBtn").on("click", function (e) {
         e.preventDefault();
 
@@ -174,6 +244,9 @@ function loadPoolInfo() {
         $("#connectBtn").show();
         $("#userName").text("Anonymous");
     });
+
+    $("#openModalBtn").prop("disabled", false);
+
     $("#openModalBtn").on("click", function (e) {
         e.preventDefault();
 
@@ -197,74 +270,9 @@ function loadPoolInfo() {
             doError("Amount cant be empty.");
         }
     });
-    
-
-    $("#origSelected").on("change", function (e) {
-        $("#title").data("coin", ($(this).val() == "bitcoin") ? "stacks" : "bitcoin");
-        $("#changeBtn").trigger("click");
-    });
-    $("#destSelected").on("change", function (e) {
-        $("#title").data("coin", ($(this).val() == "bitcoin") ? "bitcoin" : "stacks");
-        $("#changeBtn").trigger("click");
-    });
-    $("#confirmBtn").on("click", function (e) {
-        e.preventDefault();
-
-        let amountOrig = parseFloat($("#origAmount").val()) * 100000000;
-
-        try {
-            window.LeatherProvider.request("sendTransfer", {
-              recipients: [
-                {
-                  address: $("#PoolBtcAddress").val(),
-                  amount: amountOrig,
-                }
-              ],
-              network: "testnet",
-            }).then(function (response) {
-
-                alert(response.result.txid);
-                console.log("Transaction ID:", response.result.txid);
-
-                let vOrigAmout = parseFloat($("#origAmount").val()) * 100000000;
-                let vDestAmout = parseFloat($("#destAmount").val()) * 100000000;
-
-                let txParam = {
-                    btcToStx: true,
-                    btcAddress: $("#BtcAddress").val(),
-                    stxAddress: $("#StxAddress").val(),
-                    btcTxid: response.result.txid,
-                    stxTxid: null,
-                    btcAmount: vOrigAmout,
-                    stxAmount: vDestAmout
-                };
-                $.ajax({
-                    url: API_URL + "/api/Transaction/createTx",
-                    type: "PUT",
-                    data: JSON.stringify(txParam),
-                    contentType: "application/json; charset=utf-8",
-                    dataType: "json",
-                    success: function(response) {
-                        alert(JSON.stringify(response));
-                        console.log(response);
-                    },
-                    error: function(xhr, status, error) {
-                        doError(xhr.responseText);
-                        $("#openModalBtn").prop("disabled", true);
-                    }
-                });
-                //$("#btcAddress").val()
-            });
-          
-            //console.log("Response:", response);
-            //console.log("Transaction ID:", response.result.txid);
-          } catch (error) {
-            doError("Request error: " + error.error.message + "(" + error.error.code + ")");
-          }
-    });
 
     //loadPrice();
-    loadPoolInfo();
+    loadSwapForm();
       
 
  })(jQuery)
