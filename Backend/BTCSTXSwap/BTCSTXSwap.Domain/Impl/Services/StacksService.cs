cf538815e653af0a6a4cabe802e734c41dde353a
Rodrigo Landim <rodrigo.carneiro@benner.com.br>
2024-10-19 18:53:44 -0300

    Wallet STX fixed

--- Backend/BTCSTXSwap/BTCSTXSwap.Domain/Impl/Services/StacksService.cs
+++ Backend/BTCSTXSwap/BTCSTXSwap.Domain/Impl/Services/StacksService.cs
@@ -12,7 +12,8 @@ namespace BTCSTXSwap.Domain.Impl.Services
 {
     public class StacksService: IStacksService
     {
-        private const string WALLET_API = "http://localhost:3000";
+        //private const string WALLET_API = "http://localhost:3000";
+        private const string WALLET_API = "http://167.172.240.71:3000";
         private const string STACKS_API = "https://api.testnet.hiro.so/extended";
 
         public async Task<long> GetBalance(string stxAddress)
--- WalletSTX/install_docker.bat
+++ WalletSTX/install_docker.bat
@@ -1,2 +1,2 @@
 docker build -t stacks-wallet .
-docker run -p 3000:3000 stacks-wallet
+docker run --name wallet-stx -p 3000:3000 stacks-wallet
